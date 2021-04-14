b3cc50a424e79ee13969ecd2c2658e1641a57ed2
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-04-14 11:15:05 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import './assets/css/style.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>MoneyTracker App</h1>
+    </div>
+  );
+}
+
+export default App;
