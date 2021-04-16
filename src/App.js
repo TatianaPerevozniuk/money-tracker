dea8248ebc39f55f5f54e96fe8311915410bea81
marychain <prettmary19@gmail.com>
2021-04-16 12:55:18 +0300

    adding changes by marychain

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import './App.css';
 import './assets/css/style.css';
+import Charges from './components/Charges';
 
 function App() {
   return (
     <div className="App">
       <h1>MoneyTracker App</h1>
     </div>
+    <Charges />
   );
 }
 
