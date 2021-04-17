9335a17077793855344f4a12e4a1fa1f913a09b1
TatianaPerevozniuk <tatiana012494@gmail.com>
2021-04-17 15:44:32 +0300

    added layout

--- src/App.js
+++ src/App.js
@@ -5,11 +5,9 @@ import Charges from './components/Charges';
 
 function App() {
   return (
-    <div className="App">
-        <h1>Hello</h1>
-      <h2>MoneyTracker App2</h2>
+      <div className="container-fluid">
+
     </div>
-    <Charges />
   );
 }
 
